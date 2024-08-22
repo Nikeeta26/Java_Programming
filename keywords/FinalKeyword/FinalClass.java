b4c506e83712df76a9e7e42f78f1e42016808ad1
Nikeeta26 <151011330+Nikeeta26@users.noreply.github.com>
2024-08-22 22:44:59 +0530

    add two new ex

---
+++ keywords/FinalKeyword/FinalClass.java
@@ -0,0 +1,13 @@
+
+// If you make any class as final, you cannot extend it.
+
+final class Parent
+{ }
+
+public class FinalClass extends Parent {
+   public static void main(String[] args) {
+
+       System.out.println("Hello world");
+  }
+
+}
