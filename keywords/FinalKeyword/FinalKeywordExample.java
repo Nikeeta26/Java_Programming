369321360bce85d718fc2350286de4c4c8c466b9
Nikeeta26 <151011330+Nikeeta26@users.noreply.github.com>
2024-08-25 18:41:20 +0530

    add

--- keywords/FinalKeyword/FinalKeywordExample.java
+++ keywords/FinalKeyword/FinalKeywordExample.java
@@ -1,13 +1,11 @@
-
-// If you make any class as final, you cannot extend it.
-
-final class Parent
-{ }
-
-public class FinalKeywordExample extends Parent {
-   public static void main(String[] args) {
-
-       System.out.println("Hello world");
+// If you make any variable as final, you cannot change the value of final variable(It will be constant)
+ public class sample {
+    final inta=10;//final variable  
+    void display() {  
+      a=20;  //error
+    }  
+public static void main(String[] args) {
+    sample s=new sample();  
+    s.display();
   }
-
-}
+ }
