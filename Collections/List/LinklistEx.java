798180ce4627cf384f62192b6d9191fbd805ab31
Nikeeta26 <151011330+Nikeeta26@users.noreply.github.com>
2024-08-31 20:19:31 +0530

    create linklistex

---
+++ Collections/List/LinklistEx.java
@@ -0,0 +1,5 @@
+// LinkedList :-
+// it can contain duplicate elements.
+// it maintain insertion order.
+// it allows random access because it works on index basis.
+// manupulation is little is slower because lots of bit shifting requires if any element is removed from array list.
