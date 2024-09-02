927d88514c383e115d1c15f013687bb83ebc2e8b
Nikeeta26 <151011330+Nikeeta26@users.noreply.github.com>
2024-09-02 20:34:28 +0530

    add program

--- Arrays/twoDime.java
+++ Arrays/twoDime.java
@@ -16,8 +16,9 @@ int [][] arr = new int [row][col];
     
     for(int i=0; i<row; i++){
         for(int j=0; j<col; j++){
-          System.out.println(arr[i]+" "+[j]);
+          System.out.print(arr[i]+" "+[j]);
         }
+        System.out.println();
     }
     
  }
