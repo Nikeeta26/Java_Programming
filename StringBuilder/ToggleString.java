a0478e86fc1c93faf49bb99f57902d094c9b1488
Nikeeta26 <151011330+Nikeeta26@users.noreply.github.com>
2024-09-06 21:30:07 +0530

    add  new program

--- StringBuilder/ToggleString.java
+++ StringBuilder/ToggleString.java
@@ -2,6 +2,33 @@ package nikeeta;
 
 import java.util.Scanner;
 
-public class StringBuilder {
+public class ToggleString {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the elements");
+        int n = sc.nextInt();
+        
+        String binaryString = Integer.toBinaryString(n);
+        		System.out.println(binaryString);
+        			
+        		StringBuilder toggledBinaryString = new StringBuilder();
+        		
+        		 for (int i = 0; i < binaryString.length(); i++) {
+        	            if (binaryString.charAt(i) == '1') {
+        	            	
+        	                toggledBinaryString.append('0');
+        	                
+        	            } else {
+        	            	
+        	                toggledBinaryString.append('1');
+        	                
+        	            }
+        	        }
+        		 
+        		 int result = Integer.parseInt(toggledBinaryString.toString(),2);
+        		 System.out.println(result);
+	}
+
+}
