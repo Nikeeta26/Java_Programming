bcc8d360178eb33ee3f83366be8957aa6dae931b
Nikeeta26 <151011330+Nikeeta26@users.noreply.github.com>
2024-09-07 21:26:19 +0530

    add operators

--- StringBuilder/StringBuilder.java
+++ StringBuilder/StringBuilder.java
@@ -2,31 +2,33 @@ package nikeeta;
 
 import java.util.Scanner;
 
-public class StringBuilder {
+public class StringBui {
 
 	public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder("nikeeta");
-        System.out.println(sb);
-          System.out.println(sb.charAt(0));
-     
-    //set char at index
-   sb.setCharAt(0,"p");
-      System.out.println(sb);
+		// TODO Auto-generated method stub
 
-    //insert char
-    sb.insert(0,"n")
-    System.out.println(sb);
+		        StringBuilder sb = new StringBuilder("nikeeta");
+		        System.out.println(sb);
+		          System.out.println(sb.charAt(0));
+		     
+		    //set char at index
+		          sb.setCharAt(1, 'x');  
+		          System.out.println("new string = " + sb);  
+		          
+		    //insert char
+		    sb.insert(0,"n");
+		    System.out.println(sb);
 
-    //delete 
-    sb.delete(2,3);
+		    //delete 
+		    sb.delete(2,4);
 
-    //append 
-    sb.append("n");
-     sb.append("n");
-    
-    System.out.println(sb);
+		    //append 
+//		    sb.append("n");
+//		     sb.append("n");
+		    
+		    System.out.println(sb);
 
-    //legnth
-    System.out.println(sb.legnth());
-    }
-}
+		    //legnth
+		    System.out.println(sb.length());
+		    }
+		}
