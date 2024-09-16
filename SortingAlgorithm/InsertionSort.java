e2c4a0363be8759629e1fc4038f0a465717d255a
Nikeeta26 <151011330+Nikeeta26@users.noreply.github.com>
2024-09-16 20:32:22 +0530

    create insertion sort

--- SortingAlgorithm/InsertionSort.java
+++ SortingAlgorithm/InsertionSort.java
@@ -15,14 +15,14 @@ public class InsertionSort {
 
         int arr[] = {5, 3, 4, 5, 6, 7};
         for(int i=1; i<arr.length; i++){
-            int current = i;
+            int current = arr[i];
             int j = i-1;
             while(j>=0 && current<arr[j] ){
                 arr[j+1] = arr[j];
                 j--;
             }
 
-            arr[j+1] = arr[j];
+            arr[j+1] = current;
         }
 
         for(int i : arr){
