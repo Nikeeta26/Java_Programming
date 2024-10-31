f08dd77e4cef884ed63781eb0e7cf4ce885c87ab
Nikeeta26 <151011330+Nikeeta26@users.noreply.github.com>
2024-10-31 13:50:00 +0530

    Update BTS.java

--- Collections/Tree/BTS.java
+++ Collections/Tree/BTS.java
@@ -11,20 +11,48 @@ public class BTS {
 		}
 	}
 	
-	public static Node insert(Node root, int data) {
+	public static Node insert(Node root, int data) {  // O(H)
 		if(root == null) {
 			root = new Node(data);
 			return root;
 		}
 		
 		if(root.data >= data) {
-			insert(root.left, data);
+			root.left = insert(root.left, data);
+		}
+		
+		else {
+			root.right = insert(root.right,data);
 		}
 		return root;
 		
 	}
+	
+	public static void Inoder(Node root) {
+		 if (root == null) {
+	            return;
+	        }
+		
+		 Inoder(root.left);
+		 
+		 System.out.println(root.data);
+	
+		 Inoder(root.right);
+		 
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+int arr[] = {1,2,3,4,6,8,0,4};
+
+Node root = null;
+
+for(int i=0; i<arr.length; i++) {
+	root = insert(root, arr[i]);
+	
+}
+
+Inoder(root);
 
 	}
 
