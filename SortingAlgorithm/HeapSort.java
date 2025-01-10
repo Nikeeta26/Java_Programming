d2031112b8df21301c02637d617daffc390a4138
Nikeeta26 <151011330+Nikeeta26@users.noreply.github.com>
2025-01-10 21:05:35 +0530

    Create HeapSort.java

---
+++ SortingAlgorithm/HeapSort.java
@@ -0,0 +1,76 @@
+package nikeeta;
+
+public class HeapSort {
+
+    // Function to heapify a subtree rooted at node i
+    void heapify(int arr[], int n, int i) {
+        int largest = i; // Initialize largest as root
+        int left = 2 * i + 1; // Left child
+        int right = 2 * i + 2; // Right child
+
+        // If left child is larger than root
+        if (left < n && arr[left] > arr[largest]) {
+            largest = left;
+        }
+
+        // If right child is larger than the largest so far
+        if (right < n && arr[right] > arr[largest]) {
+            largest = right;
+        }
+
+        // If the largest is not root
+        if (largest != i) {
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+
+            // Recursively heapify the affected subtree
+            heapify(arr, n, largest);
+        }
+    }
+
+    // Main function to sort an array using heap sort
+    void sort(int arr[]) {
+        int n = arr.length;
+
+        // Build heap (rearrange array)
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        // One by one extract an element from the heap
+        for (int i = n - 1; i > 0; i--) {
+            // Move current root to end
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // Call heapify on the reduced heap
+            heapify(arr, i, 0);
+        }
+    }
+
+    static // Utility function to print an array
+    void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        HeapSort ob = new HeapSort();
+
+        int arr[] = {9 ,89, 6 ,5 ,4 ,0 ,23 ,56, 76 };
+        System.out.println("unsorted");
+        printArray(arr);
+        
+        System.out.println("sorted");
+        ob.sort(arr);
+        
+        printArray(arr);
+        
+        
+      }
+}
